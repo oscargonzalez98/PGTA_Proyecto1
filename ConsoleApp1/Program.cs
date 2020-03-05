70d721c3c3d46246c99df8b6757f5bfc1ea71f3b
oscargonzalez98 <oscar.go.martin@gmail.com>
2020-03-05 14:31:49 +0100

    cosa1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Clases;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+using LibreriaClases;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<CAT10> listaCAT10 = new List<CAT10>();
+            List<CAT21> listaCAT21 = new List<CAT21>();
+
+            string path= "C:/Users/oscar/Dropbox/Oscar/Sistemes Aeroespacials/4A/PGTA/Proyecto1/Proyecto/AST/adsb_v21_bcn.ast";
+            Fichero newFichero = new Fichero(path);
+            newFichero.leer();
+            listaCAT21=newFichero.GetListCAT21(); // devuelve lista de clases CAT21, cada una con un paquete
+
+            
+
+            ///// esto ira dentro de un while (al final no, pero no tocar)
+            CAT21 newCAT21 = listaCAT21[3];
+            string[] paquete = newCAT21.paquete;
+            newCAT21.Calculate_FSPEC(paquete);
+            int i = paquete.Length;
+            string FSPEC=newCAT21.Calculate_FSPEC(paquete);
+
+
+            i = 1;
+
+
+
+        }
+    }
+}
